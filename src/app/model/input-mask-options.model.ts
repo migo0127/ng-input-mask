56be73cf69f4d0c70bd50e4827274d6d490e2638
migo0127 <migo0127@gmail.com>
2023-11-11 00:39:14 +0800

    feat(input-mask-options): 更新註釋

--- src/app/model/input-mask-options.model.ts
+++ src/app/model/input-mask-options.model.ts
@@ -2,6 +2,6 @@ export interface IInputMaskOptions {
   show: boolean; // 明、隱碼切換
   sIndex: number; // 要替換的起始位置，從 0 起，
   cut: number; // 要替位幾個數字
-  symbol?: string; // 替換符號 * ● ...等
+  symbol?: string; // 替換符號 * ● ...等，默認 '*'
 }
 
