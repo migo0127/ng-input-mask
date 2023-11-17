3cec758c720d6d031e1cecebc239b4caf0f96071
migo0127 <migo0127@gmail.com>
2023-11-18 00:35:19 +0800

    feat(input-mask.d): 邏輯重新調整 - temp: 改存全部明碼

--- src/app/share/input-mask.directive.ts
+++ src/app/share/input-mask.directive.ts
@@ -14,11 +14,11 @@ export class InputMaskDirective implements ControlValueAccessor, OnChanges {
 
   @Input('inputMaskD') maskOptions: IInputMaskOptions;
 
-  tempMask: string[] = [];
+  temp: string[] = [];
 
   ngOnChanges(): void {
     // console.log('ngOnChanges: ', this.maskOptions);
-    const replaceValue: IReplaceValueData = this.replaceText(this.erf.nativeElement, 'change');
+    const replaceValue: IReplaceValueData = this.replaceText(this.erf.nativeElement);
     if(this.erf.nativeElement && replaceValue){
       this.setValue(this.erf.nativeElement, replaceValue);
     }
@@ -50,93 +50,75 @@ export class InputMaskDirective implements ControlValueAccessor, OnChanges {
 
   @HostListener('input', ['$event'])
   onInput(e: Event): void{
-    const replaceValue: IReplaceValueData = this.replaceText(e, 'input');
+    // console.log('onInput', this.maskOptions);
+    const replaceValue: IReplaceValueData = this.replaceText(e);
     if(e && replaceValue){
       this.setValue(e, replaceValue);
     }
   }
 
-  private replaceText(e: Event | ElementRef<HTMLInputElement> | null, active: string): IReplaceValueData {
+  // 保存暫存的明碼及替換成隱碼
+  private replaceText(e: Event | ElementRef<HTMLInputElement> | null): IReplaceValueData {
+    // 默認 symbol 為 '*'
     this.maskOptions.symbol = this.maskOptions?.symbol ? this.maskOptions.symbol : '*';
-    const inpElement: HTMLInputElement | null = this.getElement(e);
+    const { symbol } = this.maskOptions;
+    let { sIndex, cut } = this.maskOptions;
+    const inpElement: HTMLInputElement | null = this.getHTMLInputElement(e);
+    let inputValue: string = inpElement?.value ?? '';
+    // 取得滑鼠當前所在位置
     const selectionEnd = inpElement?.selectionEnd || 0;
-    const isDelete: boolean = (e as InputEvent)?.inputType?.includes('Backward');
-    const isInert: boolean = !isDelete && (this.maskOptions.sIndex >= selectionEnd - this.maskOptions.cut &&
-      selectionEnd - this.maskOptions.cut < this.maskOptions.sIndex + this.maskOptions.cut);
-    let displayValue: string = '';
-    if(this.maskOptions?.rexExp && inpElement?.value && (active === 'input' || this.maskOptions.update)){
-      displayValue = inpElement?.value.replace(this.maskOptions.rexExp, '') || '';
-    }else{
-      displayValue = inpElement?.value ?? '';
-    }
 
-    const isMaskAreaEntry: boolean = selectionEnd !== 0 && selectionEnd !== displayValue.length;
-    const text: string[] = Array.from(displayValue);
-    let temp: string = '';
+    // 取得 delete 及 insert 事件，並保存該值 data
+    const inputEvent: InputEvent | null = e as InputEvent || null;
+    const data: string | null = inputEvent ? inputEvent.data : null;
+    const isDelete: boolean = inputEvent?.inputType?.includes('delete');
+    const isInsert: boolean = inputEvent?.inputType?.includes('insert');
 
-    // console.log(isMaskAreaEntry, selectionEnd, displayValue.length, isDelete, isInert);
+    // 將當前輸入框的值轉成 array
+    const text: string[] = Array.from(inputValue);
+    let maskValue: string[] = [];
 
-    if(displayValue){
-      // 當輸入的字元位於隱碼區域內
-      if(isMaskAreaEntry && active === 'input'){
-        if(isDelete){
-          // 將隱碼刪除
-          this.tempMask.splice(selectionEnd - this.maskOptions.cut, 1);
-        }else if(isInert){
-          // 在隱碼區域新增字元
-          // 須先將新增字元加入對應的暫存的 tempMask 裡
-          const idx: number = selectionEnd - this.maskOptions.sIndex - 1;
-          this.tempMask.splice(idx, 0, (e as InputEvent)?.data || '');
-          // 在將所有暫存的字元（tempMask）替換回 text 裡，使 text 變成全明碼的狀態
-          this.updateTextWithMask(displayValue, text);
+    // console.log('status: ', {selectionEnd, data, isDelete, isInsert});
+
+    if(inputValue){
+      if(isDelete){
+        // 當點擊刪除時，將暫存的相對位置的值刪除
+        this.temp.splice(selectionEnd, 1);
+      }else if(isInsert && data) {
+        // 當新增字元時，將值新增進暫存的相對位置
+        this.temp.splice(selectionEnd - 1, 0, data);
+      }
+
+      for(let i = 0; i < text.length; i++){
+        if(text[i] !== symbol){
+          // 將明碼全部暫存起來
+          this.temp[i] = text[i];
         }
       }
 
-      // 提取要被改成 symbol 的文字，暫存在 tempMask 裡
-      for (let i = 0; i < this.maskOptions.cut; i++) {
-        if (text[this.maskOptions.sIndex + i] != this.maskOptions.symbol) {
-          this.tempMask[i] = <string>text[this.maskOptions.sIndex + i];
-        }
-      }
+      // 將明碼賦值給隱碼變數
+      maskValue = [...this.temp];
 
-      this.tempMask = this.tempMask.filter((v: string) => v);
-      // console.log('tempMask: ', this.tempMask);
-
-      // 提取 symbol 前後區域的明碼
-      const front = displayValue.substring(0, this.maskOptions.sIndex) || '';
-      const back = displayValue.substring(this.maskOptions.sIndex + this.maskOptions.cut) || '';
-      // 重新組合名碼文字
-      temp = front + this.tempMask.join('') + back;
-
-      // 將只定區域的文字改成 symbol
-      for (let i = 0; i < this.maskOptions.cut; i++) {
-        if (text[this.maskOptions.sIndex + i]) {
-          text[this.maskOptions.sIndex + i] = this.maskOptions.symbol
+      // 將隱碼變數指定區域的文字改成 symbol
+      for (let i = 0; i < cut; i++) {
+        if (maskValue[sIndex + i]) {
+          maskValue[sIndex + i] = symbol
         }
       }
     }else{
-      this.tempMask = [];
+      this.temp = [];
     }
 
+    // console.log('value: ', { temp: this.temp, text: text, maskValue: maskValue });
     return {
-      displayValue: temp,
-      maskValue: text.join('')
+      displayValue: this.temp.join(''),
+      maskValue: maskValue.join('')
     }
   }
 
-  // 在將所有暫存的字元（tempMask）替換回 text 裡，使 text 變成全明碼的狀態
-  private updateTextWithMask(displayValue: string, text: string[]): void {
-    for(let i = 0; i < displayValue.length; i++){
-      if(i >= this.maskOptions.sIndex && i <= this.maskOptions.sIndex + this.maskOptions.cut){
-          text.splice(i, 1, this.tempMask[i - this.maskOptions.sIndex]);
-      }
-    }
-    // 在將多暫存的字元移除，保持相對應的 cut 所儲存的 N 位
-    this.tempMask.splice(this.tempMask.length - 1, 1);
-  }
-
+  // 分別將明碼及隱碼的值賦值 formControl(明) 及 HTMLInputElement(明/隱)
   private setValue(e: ElementRef<HTMLInputElement> | Event, replaceValue: IReplaceValueData): void {
-    const inpElement: HTMLInputElement | null = this.getElement(e);
+    const inpElement: HTMLInputElement | null = this.getHTMLInputElement(e);
 
     // 將值塞入 element 的 value 屬性裡，這裡只會顯示，不會連動到 formControl 的值
     if(this.maskOptions.show){
@@ -153,14 +135,16 @@ export class InputMaskDirective implements ControlValueAccessor, OnChanges {
       );
     }
 
-    // this.onChange 賦值給 formControl 的值，不會連動到 HTMLInputElement 的 value
+    // console.log('onChange', [replaceValue.displayValue, this.ngControl.value]);
+
+    // 當點擊顯隱碼時，不需要觸發 formControl 賦值
     if(replaceValue.displayValue !== this.ngControl.value){
-      // console.log('onChange', [replaceValue.displayValue, this.ngControl.value]);
+      // this.onChange 是賦值給 formControl 的值，不會連動到 HTMLInputElement 的 value
       this.onChange(replaceValue.displayValue);
     }
   }
 
-  private getElement(e: Event | ElementRef<HTMLInputElement> | null): HTMLInputElement | null {
+  private getHTMLInputElement(e: Event | ElementRef<HTMLInputElement> | null): HTMLInputElement | null {
     return (e instanceof HTMLInputElement) ? e : (e as Event).target as HTMLInputElement;
   }
 
